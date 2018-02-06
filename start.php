571fbf70445a9570c7069c46f04357d569d360e2
Vasily Pirajog <33278849+wnull@users.noreply.github.com>
2018-02-06 14:29:03 +0300

    Update start.php

--- start.php
+++ start.php
@@ -6,7 +6,6 @@ $access_token = 'user_token';
 $user_id = 1; // your id vk
 
 $foot = (new VKUniverse)->count_wall($user_id, 'owner', $access_token); // all || owner
-
-var_dump($foot); // dump json
+echo $foot;
 
 ?>
